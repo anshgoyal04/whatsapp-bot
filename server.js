29bdec65ed3f8da07f23ece67d55f6a3d6dac7e6
Ansh <anshhgoyal04gmail.com>
2026-05-20 12:04:35 -0700

    Normalize line endings

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.get("/", (req, res) => {
   res.send("WhatsApp Bot Running");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `Server running on port ${process.env.PORT}`
-  );
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
