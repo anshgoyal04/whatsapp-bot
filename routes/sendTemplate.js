d93c943291264d9c191f471552815758f82150b4
Ansh <anshhgoyal04gmail.com>
2026-05-27 05:10:05 -0700

    template send

--- routes/sendTemplate.js
+++ routes/sendTemplate.js
@@ -5,79 +5,89 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
 
-  try {
+    try {
 
-    const number = req.query.number;
+        const number = req.query.number;
 
-    // CHECK NUMBER
-    if (!number) {
+        // CHECK NUMBER
+        if (!number) {
 
-      return res.send(
-        "Please provide WhatsApp number"
-      );
-    }
-
-    const response = await axios.post(
-
-      `https://graph.facebook.com/v19.0/${process.env.PHONE_NUMBER_ID}/messages`,
-
-      {
-        messaging_product: "whatsapp",
-
-        to: number,
-
-        type: "template",
-
-        template: {
-
-  name: "navnoor_lead_flow",
-
-  language: {
-    code: "en"
-  },
-
-//   components: [
-//     {
-//       type: "header",
-
-//       parameters: [
-//         {
-//           type: "image",
-
-//           image: {
-//             link: "https://www.navnoor.co/assets/img3.jpeg"
-//           }
-//         }
-//       ]
-//     }
-//   ]
-}
-      },
-
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.TOKEN}`,
-          "Content-Type": "application/json"
+            return res.send(
+                "Please provide WhatsApp number"
+            );
         }
-      }
-    );
 
-    console.log("Template sent");
+        const response = await axios.post(
 
-    console.log(response.data);
+            `https://graph.facebook.com/v19.0/${process.env.PHONE_NUMBER_ID}/messages`,
 
-    res.send("Template sent successfully");
+            {
+                messaging_product: "whatsapp",
 
-  } catch (err) {
+                to: number,
 
-    console.log(
-      err.response?.data || err.message
-    );
+                type: "template",
 
-    res.status(500).send(
-      "Error sending template"
-    );
-  }
+                template: {
+
+                    name: "navnoor_lead_flow",
+
+                    language: {
+                        code: "en"
+                    },
+                    components: [
+                        {
+                            type: "button",
+
+                            sub_type: "flow",
+
+                            index: "0",
+
+                            parameters: []
+                        }
+                    ]
+                    //   components: [
+                    //     {
+                    //       type: "header",
+
+                    //       parameters: [
+                    //         {
+                    //           type: "image",
+
+                    //           image: {
+                    //             link: "https://www.navnoor.co/assets/img3.jpeg"
+                    //           }
+                    //         }
+                    //       ]
+                    //     }
+                    //   ]
+                }
+            },
+
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.TOKEN}`,
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+
+        console.log("Template sent");
+
+        console.log(response.data);
+
+        res.send("Template sent successfully");
+
+    } catch (err) {
+
+        console.log(
+            err.response?.data || err.message
+        );
+
+        res.status(500).send(
+            "Error sending template"
+        );
+    }
 });
 
 module.exports = router;
